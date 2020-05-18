dbc029ce1ced4ff597807fcb7725c41f1f970e38
Alican Uver <can.uverr@gmail.com>
2020-05-18 23:12:08 +0300

    written methods that add todos to firebase

--- Src/javascript/Modules/mainTodo.js
+++ Src/javascript/Modules/mainTodo.js
@@ -1,31 +1,37 @@
 export class MainTodo {
-    constructor() {
+    constructor(config) {
         //! Select Elements
         //Buttons
-        this.detailTodo = document.getElementById("detail-todo")
-        this.addTodo = document.getElementById("add-todo")
-        this.editTodo = document.getElementById("edit-todo")
-        this.deleteTodo = document.getElementById("delete-todo")
+        this.detailTodo = document.getElementById("detail-todo");
+        this.addTodo = document.getElementById("add-todo");
+        this.editTodo = document.getElementById("edit-todo");
+        this.deleteTodo = document.getElementById("delete-todo");
+        this.logoutButton = document.getElementById("logout-button");
 
-        // Positions of todo UI
+        // UI Elements
         this.todoDetails = document.querySelector(".todo-center");
         this.todoBottom = document.querySelector(".todo-bottom");
+        this.navbarEmail = document.getElementById("navbar-email");
 
         // Inputs and Selects
         this.selectAction = document.getElementById("select-action");
         this.selectDate = document.getElementById("select-date");
         this.inputTitle = document.getElementById("todo-title");
         this.inputDescription = document.getElementById("todo-description");
-
-
+        
+        //Firebase 
+        this.config = firebase.initializeApp(config);
+        this.currentUser = "";
         this.init();
     }
 
     init() {
         this.showDetailTodo();
         this.getDetailValues();
+        this.checkUserLogin();
     }
 
+    // UI Functions
     showDetailTodo() {
         this.detailTodo.addEventListener("click", () => {
             this.todoDetails.classList.toggle("active");
@@ -39,7 +45,11 @@ export class MainTodo {
             const inputDescription = this.inputDescription.value;
             const selectAction = this.selectAction.value;
             const selectDate = this.selectDate.value;
-            this.addTodoToUI(inputTitle, inputDescription, selectAction, selectDate);
+
+            // If all inputs are not empty 
+            if (this.checkInputEmpty()) {
+            this.addTodoToUI(inputTitle, inputDescription, selectAction, selectDate)
+            };
         })
     }
 
@@ -58,5 +68,48 @@ export class MainTodo {
             </div>
         </div>
         `
+    }
+
+    checkInputEmpty() {
+        if (this.inputTitle.value !== "" && this.inputDescription.value !== "") {
+            return true;
+        }
+    }
+
+    // Firebase Functions 
+    checkUserLogin() {
+        firebase.auth().onAuthStateChanged((user) => { //whatever user we logged in with, that user's information will come in.
+            if (user) {
+                this.currentUser = user.uid;
+                this.navbarEmail.innerHTML = `User: ${user.email}`;
+                this.logoutFirebase();
+                this.addTodosToFirebase();
+            }
+        })
+    }
+
+
+    addTodosToFirebase() {
+        this.addTodo.addEventListener("click", () => {
+            if (this.checkInputEmpty()) {
+                firebase.database().ref("/users").child(this.currentUser).child("todos").push({
+                    action: this.selectAction.value,
+                    date: this.selectDate.value,
+                    title: this.inputTitle.value,
+                    description: this.inputDescription.value
+                })                
+            }
+        })
+    }
+
+    logoutFirebase(){
+        this.logoutButton.addEventListener("click", () => {
+            firebase.auth().signOut()
+            .then(() => {
+                window.location.href = "login.html"
+            }).catch((err) => {
+                console.log(err);
+            });
+        })
     }
 }
